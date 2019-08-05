ef4e03e7e9a5af9b348de00f6f8e99b1335c0877
Arthur <pyeye.91@gmail.com>
2019-08-05 14:35:19 +0500

    fix design sale detail page

--- pages/sales/_id.vue
+++ pages/sales/_id.vue
@@ -3,17 +3,22 @@
     <div class="header">
       <div class="title-row">
         <dub-breadcrumbs :breadcrumbs="breadcrumbs"></dub-breadcrumbs>
-        <div class="title">{{ sale.name }}</div>
-        <div class="sub-title">
-          C {{ sale.fdate_start.day }} {{ sale.fdate_start.month }}
-          по {{ sale.fdate_end.day }} {{ sale.fdate_end.month }}
-        </div>
       </div>
     </div>
     <div class="hero-row">
-      <div class="description-box">
-        <div class="description-text">{{ sale.description }}</div>
+      <div class="description">
+        <div class="description-title">
+          {{ sale.name }}
+        </div>
+        <div class="description-box">
+          <div class="description-date">
+            C {{ sale.fdate_start.day }} {{ sale.fdate_start.month }}
+            по {{ sale.fdate_end.day }} {{ sale.fdate_end.month }}
+          </div>
+          <div class="description-text">{{ sale.description }}</div>
+        </div>
       </div>
+      
       <div class="hero" :style="'background-image: url('+ sale.image.src +');'"></div>
     </div>
 
@@ -197,14 +202,16 @@ export default {
   );
 }
 .hero {
-  position: relative;
-  width: 80%;
-  margin: 0 0 0 auto;
+  position: absolute;
+  width: 65%;
+  top: 0;
+  right: 0;
+  z-index: 1;
   min-height: 400px;
   background-size: cover;
   background-repeat: no-repeat;
   background-position: 50% 50%;
-  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
 }
 .cover {
   position: absolute;
@@ -265,22 +272,49 @@ export default {
     ),
     webkit ms
   );
-  margin-bottom: 48px;
+  position: relative;
+}
+.description {
+  @include prefix(
+    (
+      display: flex,
+      flex-direction: column,
+    ),
+    webkit ms
+  );
+  width: 35%;
+  margin-left: 5%;
+}
+.description-title {
+  padding: 16px 20% 16px 24px;
+  font-family: 'Roboto', sans-serif;
+  opacity: 0.7;
+  color: $text_color;
+  font-size: 32px;
+  line-height: 36px;
+  font-weight: 600;
+  text-transform: uppercase;
+  letter-spacing: 2px;
 }
 .description-box {
-  width: 30%;
-  transform: translate(25%, 48px);
   background-color: $upper_layer_color;
-  z-index: 1;
-  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
+  min-height: 300px;
+}
+.description-date {
+  padding: 24px 20% 8px 24px;
+  font-size: 16px;
+  line-height: 16px;
+  font-weight: 600;
+  letter-spacing: 0.05em;
+  font-family: 'Roboto', sans-serif;
+  opacity: 0.6;
+  color: $text_color;
+  text-transform: uppercase;
 }
 .description-text {
-  padding: 24px;
+  padding: 16px 20% 24px 24px;
   opacity: 0.8;
-}
-.description-text::first-letter {
-  font-size: 24px;
-  font-weight: 600;
 }
 .icon-link {
   position: absolute;
